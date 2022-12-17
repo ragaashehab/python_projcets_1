a1cc8440f8622c485038bcb2d7265efdb9ef5af8
ragaashehab <rashehab@yahoo.com>
2022-12-17 15:55:36 +0200

    first commit

---
+++ email_slicer/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    mail= list(input("enter your mail").strip().split("@"))
+    print("user name: ", mail[0], " domain name: ", mail[1])
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
